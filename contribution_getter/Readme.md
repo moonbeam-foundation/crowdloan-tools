bef17c1f9033f5ebe6a24ed81eb04ecb2d158fc7
Gorka Irazoqui <gorka.irazoki@gmail.com>
2021-06-02 10:44:48 +0200

    contribution-getter tool

---
+++ contribution_getter/Readme.md
@@ -0,0 +1,37 @@
+# Contribution-getter
+
+A contribution-getter tool for a crowdloan campaign. Contributions are stored in a child-trie and this tool helps retriving the information of the contributors and amount contributed. 
+
+## Install dependencies
+From the Moonbeam root, run
+
+`npm install`
+
+## Run the script
+The script accepts three inputs fields:
+- `--parachain-id or -p`, which specifies the parachain id associated with the crowdloan from which we want to retrieve contributions
+- `--ws-provider or -w`, which specifies the websocket provider to which we will be issuing our requests
+- `--output-dir or -o`, which specifies the output directory where we want to store our result JSON object
+
+### Example
+`npm run contribution-getter -- --parachain-id 2001 --ws-provider "ws://127.0.0.1:3333" -o "./contributions.json"`
+
+### Output format
+```json
+{
+  total_raised: 300000000000000,
+  contributions: [
+    {
+      account: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
+      contribution: 100000000000000,
+      memo: ''
+    },
+    {
+      account: '5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y',
+      contribution: 200000000000000,
+      memo: ''
+    }
+  ],
+  parachain_id: 2001
+}
+```
---
+++ contribution_getter/contribution_getter.ts
@@ -0,0 +1,76 @@
+// Import
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import {u8aToHex} from '@polkadot/util';
+import {encodeAddress} from '@polkadot/util-crypto'
+
+import {blake2AsHex} from '@polkadot/util-crypto';
+import yargs from 'yargs';
+import writeJsonFile from 'write-json-file';
+
+const args = yargs.options({
+    'parachain-id': { type: 'number', demandOption: true, alias: 'p' },
+    'ws-provider': {type: 'string', demandOption: true, alias: 'w'},
+    'output-dir': {type: 'string', demandOption: true, alias: 'o'}
+  }).argv;
+
+// Construct
+const wsProvider = new WsProvider(args['ws-provider']);
+
+async function main () {
+    const api = await ApiPromise.create({ provider: wsProvider });
+
+    // First we retrieve the trie index of the parachain-id fund info
+    const fund_info = (await api.query.crowdloan.funds(args["parachain-id"])).toJSON();
+    
+    // Second we calculate the crowdloan key. This is composed of
+    // b":child_storage:default:" + blake2_256(b"crowdloan" + trie_index as u32)
+    let bytes = new TextEncoder().encode("crowdloan");
+    // We allocate 4 bytes for the trie index
+    let buf = Buffer.allocUnsafe(4);
+    buf.writeUInt32LE(fund_info["trieIndex"]);
+    var array = Uint8Array.from(buf);
+    var concatArray = new Uint8Array([ ...bytes, ...array]);
+    let child_encoded_bytes = u8aToHex(new TextEncoder().encode(":child_storage:default:"));
+    let crowdloan_key = child_encoded_bytes + blake2AsHex(concatArray, 256).substring(2);
+
+    // Third we get all the keys for that particular crowdloan key
+    const all_keys = await api.rpc.childstate.getKeys(crowdloan_key, null);
+    let json = {
+        "total_raised" : 0,
+        "contributions" : [],
+        "parachain_id": args["parachain-id"],
+    };
+
+    // Here we iterate over all the keys that we got for a particular crowdloan key
+    for (let i = 0; i < all_keys.length; i++) {      
+        const storage = await api.rpc.childstate.getStorage(crowdloan_key, all_keys[i].toHex());
+
+        if (storage.isSome){
+            let storage_item = storage.unwrap()
+            // The storage item is composed as:
+            // Balance (16 bytes with changed endianness)
+            // 1 byte memo length
+            // Memo
+            let balance = u8aToHex(storage_item.slice(0,16).reverse())
+            let memoLenght = storage.unwrap().slice(16,17)
+            let memo = "";
+            if (u8aToHex(memoLenght) != "0x00") {
+                memo = u8aToHex(storage_item.slice(17,storage_item.length))
+            }
+            json.contributions.push({
+                "account": encodeAddress(all_keys[i].toHex()),
+                "contribution": parseInt(balance, 16),
+                "memo": memo
+            })
+            json.total_raised += parseInt(balance, 16)
+        }
+    }
+    if(json.total_raised != fund_info["raised"]){
+        throw new Error(
+            `Contributed amount and raised amount dont match`
+        );
+    }
+    console.log(json)
+    await writeJsonFile(args['output-dir'], json);
+}
+main().catch(console.error).finally(() => process.exit());
